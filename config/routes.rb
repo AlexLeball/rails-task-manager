d6db3570d3da313ada786def5f7eb2d3d38184f9
alexandra perrin <alexandrarose1@live.com.au>
2023-11-14 16:27:40 +0100

    rails crud

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,18 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  get "tasks/new", to: "tasks#new", as: :new_task
+
   get "tasks", to: "tasks#index"
+  post "tasks", to: "tasks#create"
+
   get "tasks/:id", to: "tasks#show", as: :task
-  get "tasks/new", to: "tasks#new"
+  patch "tasks/:id", to: "tasks#update"
+
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+
+  delete "tasks/:id", to: "tasks#destroy", as: :delete_task
 end
 # Defines the root path route ("/")
 # root "posts#index"
